32404a7468052651aa7c163319e78e921568ebb2
VShawn <singlex@Foxmail.com>
2015-12-05 21:24:19 +0800

    add

---
+++ TileDetectorMFC/TileDetectorMFC/SettingDlg.cpp
@@ -0,0 +1,89 @@
+// SettingDlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "TileDetectorMFC.h"
+#include "SettingDlg.h"
+#include "afxdialogex.h"
+
+
+// SettingDlg 对话框
+
+IMPLEMENT_DYNAMIC(SettingDlg, CDialogEx)
+
+SettingDlg::SettingDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(SettingDlg::IDD, pParent)
+	, set_grabMaxPics(0)
+	, set_grabWidth(0)
+	, set_grabRGBType(_T(""))
+	, set_TiggerWaitTimeMS(0)
+{
+
+}
+
+BOOL SettingDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	//读取配置文件
+	if (globle_var::InitSetting(true))
+	{
+		//设置参数
+		set_grabMaxPics = globle_var::mdi().MaxPics;
+		set_grabWidth = globle_var::mdi().width;
+		set_grabRGBType = globle_var::mdi().colorType == globle_var::mdi().RGB ? "RGB" : "Gray";
+		set_TiggerWaitTimeMS = globle_var::TiggerWaitTimeMS;
+	}
+	else
+	{
+		MessageBox(L"某些参数读取失败，请保存正确的参数信息！");
+	}
+	UpdateData(false);
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// 异常:  OCX 属性页应返回 FALSE
+}
+
+
+SettingDlg::~SettingDlg()
+{
+}
+
+void SettingDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_TB_GrabMaxPics, set_grabMaxPics);
+	DDX_Text(pDX, IDC_TB_GrabWidth, set_grabWidth);
+	DDX_CBString(pDX, IDC_DDL_GrabRGBType, set_grabRGBType);
+	DDX_Text(pDX, IDC_TB_TiggerWaitMS, set_TiggerWaitTimeMS);
+}
+
+
+BEGIN_MESSAGE_MAP(SettingDlg, CDialogEx)
+	ON_BN_CLICKED(IDC_SAVE, &SettingDlg::OnBnClickedSave)
+	ON_BN_CLICKED(IDC_Cancel, &SettingDlg::OnBnClickedCancel)
+END_MESSAGE_MAP()
+
+
+// SettingDlg 消息处理程序
+
+
+void SettingDlg::OnBnClickedSave()
+{
+	UpdateData(true);
+	//更改设置
+	globle_var::SetGrabSetting(set_grabRGBType == "RGB" ? "RGB" : "Gray", set_grabMaxPics, set_grabWidth);
+	globle_var::TiggerWaitTimeMS = set_TiggerWaitTimeMS;
+
+	//存入ini
+	globle_var::SaveSetting();
+
+
+	EndDialog(IDOK);
+}
+
+
+void SettingDlg::OnBnClickedCancel()
+{
+	EndDialog(IDCANCEL);
+}
+
---
+++ TileDetectorMFC/TileDetectorMFC/SettingDlg.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "globle_head.h"
+#include "globle_debug.h"
+#include "globle_var.h"
+#include "CvvImage.h"
+//#include "Controller/Contoller.h"
+#include "Controller/TiggerWatcherAndGrabber.h"
+#include "Controller/Consumer.h"
+
+// SettingDlg 对话框
+
+class SettingDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(SettingDlg)
+
+public:
+	SettingDlg(CWnd* pParent = NULL);   // 标准构造函数
+	virtual ~SettingDlg();
+
+// 对话框数据
+	enum { IDD = IDD_Setting };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedSave();
+	// 采集参数，总采集行数
+	int set_grabMaxPics;
+	// 采集参数，一行的采集宽度
+	int set_grabWidth;
+	// 采图时颜色类型 "RGB" or "Gray" 区分大小写
+	CString set_grabRGBType;
+	// 触发器触发后，等待多少毫秒再开始采集
+	int set_TiggerWaitTimeMS;
+	afx_msg void OnBnClickedCancel();
+	virtual BOOL OnInitDialog();
+};
