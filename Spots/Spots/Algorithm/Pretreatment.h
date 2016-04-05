797d4c37b6ebb07d064acf718619df134e040b18
dekaiidea <raindekai@hotmail.com>
2016-04-05 20:37:24 +0800

    大幅修改检测核心代码

--- Spots/Spots/Algorithm/Pretreatment.h
+++ Spots/Spots/Algorithm/Pretreatment.h
@@ -1,7 +1,7 @@
-#include <opencv2/opencv.hpp>
-//#include <opencv/highgui.h>
-//#include <opencv2/stitching/stitcher.hpp>
-//#include <opencv2/imgproc/imgproc.hpp>
+#include "..//opencv2//opencv.hpp"
+#include "..//opencv//highgui.h"
+#include "opencv2/stitching/stitcher.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <fstream>
 #include <queue>
@@ -37,29 +37,39 @@ private:
 
 	typedef struct ItemRepository ItemRepository;
 	int size;
-	Faults *_faults;
-	vector<Point> pointlist;
-	vector<Point> in_or_out;
-	vector<Point> Containpoints;
+	Faults *_faults;//各类缺陷集
+	vector<Point> pointlist;//瓷砖内部定位坐标集
+	vector<Point> pointlist_r;//原始定位坐标集
+	vector<Point> in_or_out;//最小外接矩形四顶点坐标集
+	Rect location_rect;
 	vector<Point> locationpoints;
-	vector<vector<Point>> CneedContours;
-	vector<vector<vector<Point>>> Warehousecontours;
+	vector<Rect> CneedContours;
+	vector<vector<Rect>> Warehousecontours;
 	vector<vector<Point>> Linecontours;
 	vector<vector<cv::Point>> ecliptours;
-	Mat Mask_result_big, Mask_result_small, CannyImg, BlurImg, lookUpTable,lookUpTable_main;
 	float hranges[2];
 	const float *ranges[1];
 	int channels;
+	Mat Mask_result_big, Mask_result_small, CannyImg, BlurImg, lookUpTable, lookUpTable_main;
+	
+public:
+	
+	Mat Grow(Mat &image, Point seedpoint, int th_v);//种子点区域生长
+	int otsuThreshold(Mat &frame, MatND hist);//局部寻找可靠阈值
+	Mat Equalize(Mat &_Img);//瓷砖表面预处理高斯差分滤波与直方图均衡化
+	void ProcessArea(Block *blockin);//确定瓷砖表面内部位置，A是左上，顺时针
+	int Maxdistance(vector<Point> vec);//给一组点集求出任意两点间的最大距离时间复杂度为nlogn
+	void Handwriting(Mat &_img);//判断是否存在人工笔迹
 	void img2clone();
 	void img2zoom();
+	void line2preprocess();
 	void linedetect();
-	void tem_2plate();
+	void data_import();
 	void ProduceItem(ItemRepository *ir, int item);
 	void ProducerTask();
 	int ConsumeItem(ItemRepository *ir);
 	void ConsumerTask();
 	void InitItemRepository(ItemRepository *ir);
-public:
 	void pretreatment(Mat &image, Block *_block, Faults *faults);
 	Pretreatment()
 	{
