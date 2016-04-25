99d6b3fd92fcd6a57e4d519c4a3f056604fa08d6
VShawn <singlex@Foxmail.com>
2016-04-25 14:32:32 +0800

    缺陷参数分为mm尺度与pix尺度 在Block类中记录砖的尺寸与像素/mm转换尺度

--- EdgeFaultsFinder/EdgeFaultsFinder/Algorithm/Base/Faults.h
+++ EdgeFaultsFinder/EdgeFaultsFinder/Algorithm/Base/Faults.h
@@ -24,42 +24,51 @@ public:
 	struct BrokenEdge
 	{
 		cv::Point position;//缺角位置
-		double deep;//进边深度（毫米）
-		double length;//缺边长（毫米）
+		double deep;//进边深度（pix）
+		double length;//缺边长（pix）
+		double deep_mm;//进边深度（mm）
+		double length_mm;//缺边长（mm）
 	};
 	//缺角
 	struct BrokenCorner
 	{
 		cv::Point position;//缺角位置
-		double deep;//进边深度（毫米）
-		double length;//缺角长y轴（毫米）
-		double width;//缺角宽x轴（毫米）
+		double deep;//进边深度（pix）
+		double length;//缺角长y轴（pix）
+		double width;//缺角宽x轴（pix）
+
+		double deep_mm;//进边深度（mm）
+		double length_mm;//缺角长y轴（mm）
+		double width_mm;//缺角宽x轴（mm）
 	};
 	//釉裂
 	struct Crazing
 	{
 		cv::Point position;//釉裂位置
-		double diameter;//直径（毫米）
+		double diameter;//直径（pix）
+		double diameter_mm;//直径（mm）
 	};
 	//凹点
 	struct Hole
 	{
 		cv::Point position;//凹点位置
-		double diameter;//直径（毫米）
+		double diameter;//直径（pix）
+		double diameter_mm;//直径（mm）
 	};
 	//划痕
 	struct Scratch
 	{
 		cv::Point position;//位置
-		double length;//划痕长度（毫米）
+		double length;//划痕长度（pix）
+		double length_mm;//划痕长度（pix）
 		double angel;//角度°
 	};
-	//巨大缺陷，一遇到就判断为不合格品
-	struct SomethingBig
-	{
-		cv::Point position;//位置
-		double diameter;//直径（毫米）
-	};
+	////巨大缺陷，一遇到就判断为不合格品
+	//struct SomethingBig
+	//{
+	//	cv::Point position;//位置
+	//	double diameter;//直径（pix）
+	//};
 	//人工标记
 	struct MarkPen
 	{
@@ -78,31 +87,31 @@ public:
 		Crazings.clear();//釉裂
 		Holes.clear();//凹点
 		Scratchs.clear();//划痕
-		SomethingBigs.clear();
+		//SomethingBigs.clear();
 		MarkPens.clear();
 	};
 
-	//设置参数，输入：x轴每像素对应多少毫米，y轴每像素对应多少毫米，对角线每像素对应多少毫米
-	void SetParameter(double MilliMeterPerPix_X, double MilliMeterPerPix_Y, double MilliMeterPerPix_Diagonal)
+	//设置参数，输入：x轴每像素对应多少pix，y轴每像素对应多少pix，对角线每像素对应多少pix
+	void SetParameter(double MilliMeterPermm_X, double MilliMeterPermm_Y, double MilliMeterPermm_Diagonal)
 	{
-		milliMeterPerPix_X = MilliMeterPerPix_X;
-		milliMeterPerPix_Y = MilliMeterPerPix_Y;
-		milliMeterPerPix_Diagonal = MilliMeterPerPix_Diagonal;
+		milliMeterPermm_X = MilliMeterPermm_X;
+		milliMeterPermm_Y = MilliMeterPermm_Y;
+		milliMeterPermm_Diagonal = MilliMeterPermm_Diagonal;
 	};
 
-	//获取x轴每像素对应多少毫米
-	double GetMilliMeterPerPix_X(){ return milliMeterPerPix_X; };
-	//获取y轴每像素对应多少毫米
-	double GetMilliMeterPerPix_Y(){ return milliMeterPerPix_Y; };
-	//获取对角线每像素对应多少毫米
-	double GetMilliMeterPerPix_Diagonal(){ return milliMeterPerPix_Diagonal; };
+	//获取x轴每像素对应多少pix
+	double GetMilliMeterPermm_X(){ return milliMeterPermm_X; };
+	//获取y轴每像素对应多少pix
+	double GetMilliMeterPermm_Y(){ return milliMeterPermm_Y; };
+	//获取对角线每像素对应多少pix
+	double GetMilliMeterPermm_Diagonal(){ return milliMeterPermm_Diagonal; };
 
 
 
 
 
 	/**************************属性********************************/
-	//对角线相差毫米数
+	//对角线相差pix数
 	double DiagonalDiff_mm;
 
 	vector<BrokenEdge> BrokenEdges;//缺边
@@ -110,12 +119,12 @@ public:
 	vector<Crazing> Crazings;//釉裂
 	vector<Hole> Holes;//凹点
 	vector<Scratch> Scratchs;//划痕
-	vector<SomethingBig> SomethingBigs;//巨大缺陷，一遇到就判断为不合格品
+	//vector<SomethingBig> SomethingBigs;//巨大缺陷，一遇到就判断为不合格品
 	vector<MarkPen> MarkPens;//人工标记
 private:
-	double milliMeterPerPix_X = 0;//x轴每像素对应多少毫米
-	double milliMeterPerPix_Y = 0;//y轴每像素对应多少毫米
-	double milliMeterPerPix_Diagonal = 0;//对角线每像素对应多少毫米
+	double milliMeterPermm_X = 0;//x轴每像素对应多少pix
+	double milliMeterPermm_Y = 0;//y轴每像素对应多少pix
+	double milliMeterPermm_Diagonal = 0;//对角线每像素对应多少pix
 };
 
 #endif
