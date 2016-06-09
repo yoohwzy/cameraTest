f0dd83b4eeae1c13ab310b401a03d2f3410ecfad
VShawn <singlex@Foxmail.com>
2016-06-09 15:07:33 +0800

    stringhelper添加新的方法

--- Spots/Spots/Class/Helper/StringHelper.h
+++ Spots/Spots/Class/Helper/StringHelper.h
@@ -1,48 +1,59 @@
 #pragma once
 
-#include <afx.h>
+#ifndef AFX_DATA
+//#	include <afx.h>
+#	include <afxwin.h>
+#endif
 #include <string>
 #include <iostream>
 #include <sstream>
 #include <fstream>
 
 using namespace std;
-
+#include <vector>
 class StringHelper
 {
 public:
-	static string int2string(int i)
+	static inline string int2string(int i)
 	{
 		
 		stringstream ss;
 		ss << i;
 		return ss.str();
 	}
-	static string double2string(double d)
+	static inline string double2string(double d)
 	{
 		stringstream ss;
 		ss << d;
 		return ss.str();
 	}
-	static CString int2CString(int i)
+	static inline CString int2CString(int i)
 	{
 		return string2CString(int2string(i));
 	}
-	static CString double2CString(double d)
+	static inline CString double2CString(double d)
 	{
 		return string2CString(double2string(d));
 	}
-	static string CString2string(CString cstr)
+	static inline string CString2string(CString cstr)
 	{
 		return LPWSTR2string(cstr.GetBuffer(0));
 	}
-	static CString string2CString(string str)
+	static inline CString string2CString(string str)
 	{
 		CString ret = L"";
 		ret += str.c_str();
 		return ret;
 	}
-	static string LPWSTR2string(LPWSTR lpwstr)
+	static inline int string2int(string str)
+	{
+		int value;
+		stringstream ss;
+		ss << str;
+		ss >> value;
+		return value;
+	}
+	static inline string LPWSTR2string(LPWSTR lpwstr)
 	{
 		wstring ws(lpwstr);
 		std::string curLocale = setlocale(LC_ALL, NULL);        // curLocale = "C";
@@ -57,12 +68,28 @@ public:
 		setlocale(LC_ALL, curLocale.c_str());
 		return result;
 	}
-	static LPWSTR string2LPWSTR(string str)
+	static inline LPWSTR string2LPWSTR(string str)
 	{
 		int dwLen = str.length() + 1;
 		int nwLen = MultiByteToWideChar(CP_ACP, 0, str.c_str(), dwLen, NULL, 0);//算出合适的长度
 		LPWSTR lpszPath = new WCHAR[dwLen];
 		MultiByteToWideChar(CP_ACP, 0, str.c_str(), dwLen, lpszPath, nwLen);
 		return lpszPath;
+	}
+	//注意：当字符串为空时，也会返回一个空字符串  
+	static inline void split(std::string& s, std::string& delim, vector<string>* ret)
+	{
+		size_t last = 0;
+		size_t index = s.find_first_of(delim, last);
+		while (index != std::string::npos)
+		{
+			ret->push_back(s.substr(last, index - last));
+			last = index + 1;
+			index = s.find_first_of(delim, last);
+		}
+		if (index - last>0)
+		{
+			ret->push_back(s.substr(last, index - last));
+		}
 	}
 };
