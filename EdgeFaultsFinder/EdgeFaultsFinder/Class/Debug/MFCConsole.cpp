b531979339ef81bf30ab8c41a13474665a1343f0
VShawn <singlex@Foxmail.com>
2016-06-25 19:58:32 +0800

    add

---
+++ EdgeFaultsFinder/EdgeFaultsFinder/Class/Debug/MFCConsole.cpp
@@ -0,0 +1,28 @@
+#include "MFCConsole.h"
+
+
+MFCConsole::MFCConsole()
+{
+}
+
+MFCConsole::~MFCConsole()
+{
+}
+void MFCConsole::Init()
+{
+}
+void MFCConsole::Output(stringstream msg)
+{
+
+}
+void MFCConsole::Output(string msg)
+{
+
+}
+void MFCConsole::Output(const char *msg)
+{
+}
+
+
+bool MFCConsole::IsOpened = false;
+
